ae92e4924e28104f1cdc2dea2c4b9c18da97274b
Pratik Paudel <56014021+PratikPaudel@users.noreply.github.com>
2024-05-29 23:53:47 -0500

    Update script.js

--- responsive-modal-dialog/script.js
+++ responsive-modal-dialog/script.js
@@ -31,3 +31,11 @@ overlay.addEventListener('click', function() {
       modalClose(i);
     }
   });
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
